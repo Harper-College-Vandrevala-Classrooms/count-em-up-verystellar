ff3b067ba54d828ad0ee257bb2dfa9beedad35f8
verystellar <161178496+verystellar@users.noreply.github.com>
2024-09-15 19:22:22 -0500

    counter implemented

--- src/main/java/com/csc/GroceryCounter.java
+++ src/main/java/com/csc/GroceryCounter.java
@@ -1,5 +1,94 @@
 package com.csc;
+import java.util.ArrayList;
 
 public class GroceryCounter {
-  // Put your code here!
+  int overloads = 0;
+
+  int[] nums = {0,0,0,0};
+  int[] remainder = {0,0,0,0};
+
+
+  public void callMoney(int idx){
+    switch(idx){
+        case 0: tens();
+                break;
+        case 1: ones();
+                break;
+        case 2: tenths();
+                break;
+        case 3: hundredths();
+                break;
+    }
+  }
+
+  public Boolean overload(int idx){
+    if((nums[idx]+1)> 9){
+        if(idx>0){
+            nums[idx] = 0;
+            callMoney(idx-1);
+            return true;
+        } else {
+            nums[idx] = 0;
+            overloads++;
+            return true;
+        }
+    }
+    return false;
+  }
+
+  public int number_of_overloads() {
+    return overloads;
+  }
+
+  public void tens(){
+    boolean overloaded = overload(0);
+    if (!overloaded){
+        nums[0] += 1;
+    }        
+  }
+
+  public void ones(){
+    boolean overloaded = overload(1);
+    if (!overloaded){
+        nums[1] += 1;
+
+    }        
+  }
+
+  public void tenths(){
+    boolean overloaded = overload(2);
+    if (!overloaded){
+        nums[2] += 1;
+
+    }        
+  }
+
+  public void hundredths(){
+    boolean overloaded = overload(3);
+    if (!overloaded){
+        nums[3] += 1;
+
+    }        
+  }
+
+  public String getTotal(){
+    if(nums[0] == 0){
+            String s = "$" + String.valueOf(nums[1]) + 
+                "." + String.valueOf(nums[2]) + String.valueOf(nums[3]);
+                return s;
+
+    } else {
+            String s = "$" + String.valueOf(nums[0]) + String.valueOf(nums[1]) + 
+            "." + String.valueOf(nums[2]) + String.valueOf(nums[3]);
+            return s;
+
+    }
+  }
+
+  public void clear(){
+    for(int i = 0; i < 4; i++){
+        nums[i] = 0;
+    }
+    overloads = 0;
+  }
 }
--- src/test/java/com/csc/TestGroceryCounter.java
+++ src/test/java/com/csc/TestGroceryCounter.java
@@ -10,15 +10,76 @@ public class TestGroceryCounter {
   GroceryCounter counter;
 
   @BeforeEach
-  void setUp() {
+  void setUp() {;
     counter = new GroceryCounter();
   }
 
   // Put your tests here!
 
   @Test
-  void itWorks() {
-    assertEquals(true, true);
+  void testZero() {
+    counter.ones();
+    assertEquals("$1.00", counter.getTotal());
+  }
+  @Test
+  void testOne() {
+    for(int i = 0; i < 5; i++){
+        counter.tens();
+    }
+    assertEquals("$50.00", counter.getTotal());
   }
 
+  @Test
+  void testTwo() {
+    for(int i = 0; i < 10; i++){
+        counter.tens();
+    }
+    assertEquals("$0.00", counter.getTotal());
+  }
+
+  @Test
+  void testThree() {
+    for(int i = 0; i < 10; i++){
+        counter.ones();
+    }
+    assertEquals("$10.00", counter.getTotal());
+  }
+
+  @Test
+  void testFour() {
+    for(int i = 0; i < 20; i++){
+        counter.tens();
+    }
+    int overloads = counter.number_of_overloads();
+    assertEquals(2, overloads);
+    assertEquals("$0.00", counter.getTotal());
+  }
+  @Test
+  void testFive() {
+    for(int i = 0; i < 21; i++){
+        counter.tens();
+    }
+    counter.ones();
+    counter.tenths();
+    counter.hundredths(); //"should be $11.11 and 2 overloads"
+    counter.clear();
+    assertEquals(0, counter.number_of_overloads());
+    assertEquals("$0.00", counter.getTotal());
+  }
+  @Test
+  void testSix() {
+    for(int i = 0; i < 9; i++){
+        counter.tens();
+    }
+    for(int i = 0; i < 10; i++){
+        counter.ones();
+    }
+    for(int i = 0; i < 9; i++){
+        counter.hundredths();
+    }
+    for(int i = 0; i < 9; i++){
+        counter.tenths();
+    }
+    assertEquals("$0.99", counter.getTotal());
+  }
 }
